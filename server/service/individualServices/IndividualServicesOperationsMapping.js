aeeb0564108ea7dc70546a96ff744c5a7829f4e9
ManasaBM <manasa.b-m.external@telefonica.com>
2023-02-27 06:03:36 +0100

    Including Mapping of regardandnotify

--- server/service/individualServices/IndividualServicesOperationsMapping.js
+++ server/service/individualServices/IndividualServicesOperationsMapping.js
@@ -1,9 +1,19 @@
 module.exports.individualServicesOperationsMapping =
 {
-    "/v1/regard-application": {
+   "/v1/notify-link-updates": 
+   {
+        "regard-updated-link": {
+            "api-segment": "im",
+            "sequence": "000"
+          }
+      },
+      "/v1/regard-application": 
+      {
         "redirect-topology-change-information": {
             "api-segment": "im",
             "sequence": "005"
-        }
-    }
+          }
+      }
 }
+
+
