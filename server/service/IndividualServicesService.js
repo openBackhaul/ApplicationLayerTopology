bb434b0de651aa77baed2dc3ee8a885b11951b14
Vanitha Valluripalli <vanitha.valluripalli.external@telefonica.com>
2023-03-03 13:45:56 +0100

    Removed fetching from _source  as getControlConstructAsync directly fetches it from _source

--- server/service/IndividualServicesService.js
+++ server/service/IndividualServicesService.js
@@ -428,7 +428,7 @@ exports.listEndPointsOfLink = function (body, user, originator, xCorrelator, tra
         let linkPort = linkPortList[i];
         let logicalTerminationPoint = linkPort[onfAttributes.LINK.LOGICAL_TERMINATION_POINT];
         let controlConstructUuid = figureOutControlConstructUuid(logicalTerminationPoint);
-        let controlConstruct = (await NetworkControlDomain.getControlConstructAsync(controlConstructUuid))._source;
+        let controlConstruct = (await NetworkControlDomain.getControlConstructAsync(controlConstructUuid))[0];
         linkEndPoint.operationUuid = logicalTerminationPoint;
         if (controlConstruct) {
           linkEndPoint.ltpDirection = getLtpDirection(controlConstruct, logicalTerminationPoint);
